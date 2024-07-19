c94f40748173325f6083155d7070c85a82c0dabb
Ashwin Rajaram <ashwin.rajaram@hotmail.com>
2024-07-19 22:17:13 +0800

    Add drag and drop to posts
    - Install package - tailwind, dnd kit
    - Tailwind config changes
    - Implement drag and drop with sort logic

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,11 +1,18 @@
 import { Paper } from "@mui/material";
+import { useSortable } from "@dnd-kit/sortable";
 
 
-const Post = ({ title, description, order }) => (
-    <Paper elevation={3} >
-        <h2>{order}. {title}</h2>
-        <p>{description}</p>
-    </Paper>
-);
+const Post = ({ id, title, description, order }) => {
+    const {
+        attributes,
+        listeners,
+        setNodeRef,
+    } = useSortable({ id });
+    return (
+        <Paper elevation={3} className="mt-2" ref={setNodeRef} {...attributes} {...listeners}>
+            <h2>{order}. {title}</h2>
+            <p>{description}</p>
+        </Paper>)
+};
 
 export default Post;
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,22 +1,53 @@
-import { List, ListItem, Paper, Stack } from "@mui/material";
+import { useState } from "react";
+
+import { Stack } from "@mui/material";
+import { arrayMove, SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
+import { closestCenter, DndContext, PointerSensor, useSensor, useSensors } from "@dnd-kit/core";
+
 import Post from "./Post";
 
 const PostList = ({ posts }) => {
+
+    const [postList, setPostList] = useState(posts);
+    const sensors = useSensors(
+        useSensor(PointerSensor)
+    );
+
+    const handleDragEnd = (event) => {
+        const { active, over } = event;
+        console.log(active);
+        console.log(over);
+
+        if (active.id !== over.id) {
+            setPostList((items) => {
+                const oldIndex = items.findIndex((item) => item.id === active.id);
+                const newIndex = items.findIndex((item) => item.id === over.id);
+
+                console.log(oldIndex, newIndex);
+
+                return arrayMove(items, oldIndex, newIndex);
+            });
+        }
+    };
+
     return (
         <>
-            <h1>List of Posts</h1>
-            <Stack >
-
-                {posts.map((post, index) => (
-                    <Post
-                        key={post.id}
-                        title={post.title}
-                        description={post.description}
-                        order={index + 1}
-                    />
-                ))}
-
-            </Stack>
+            <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+                <h1>List of Posts</h1>
+                <Stack >
+                    <SortableContext items={postList.map(p => p.id)} strategy={verticalListSortingStrategy}>
+                        {postList.map((post, index) => (
+                            <Post
+                                key={post.id}
+                                id={post.id}
+                                title={post.title}
+                                desciption={post.desciption}
+                                order={index + 1}
+                            />
+                        ))}
+                    </SortableContext>
+                </Stack>
+            </DndContext>
         </>
 
     );
