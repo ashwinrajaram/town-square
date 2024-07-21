0863e2955870896d50cbf6444d3f528ecfc77ec7
Ashwin Rajaram <ashwin.rajaram@hotmail.com>
2024-07-21 09:35:39 +0800

    Add animation for drag and drop
    + use dnd animation for Post component

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,17 +1,21 @@
 import { Paper } from "@mui/material";
 import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 const Post = ({ id, title, description, order }) => {
     const {
         attributes,
         listeners,
         setNodeRef,
+        transform,
+        transition
     } = useSortable({ id });
     return (
         <Paper elevation={3} className="mt-2"
             ref={setNodeRef}
             {...attributes}
             {...listeners}
+            style={{ transform: CSS.Translate.toString(transform), transition }}
         >
 
             <h2>{order}. {title}</h2>
