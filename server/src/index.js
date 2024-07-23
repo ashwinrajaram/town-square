6d1457d8d449dfc952e770503f2960b7f6ebe232
Ashwin Rajaram <ashwin.rajaram@hotmail.com>
2024-07-24 04:51:57 +0800

    Enhance security of server
    + Added configs in Apollo server constructor to prevevnt DOS attacks (recommened by Render)

--- server/src/index.js
+++ server/src/index.js
@@ -1,28 +1,14 @@
 
 import { ApolloServer } from 'apollo-server';
-// const { startStandaloneServer } = require('apollo-server-standalone');
 import typeDefs from './schema.js';
 import resolvers from './resolvers.js';
 import { closeClients } from './utils/prismaClient.js';
 
-// async function startServer() {
-//     const server = new ApolloServer({
-//         typeDefs,
-//         resolvers,
-//     });
-
-//     const { url } = await startStandaloneServer(server, {
-//         listen: { port: 4000 },
-//     });
-
-//     console.log(`🚀 Server ready at: ${url}`);
-// }
-
-// startServer();
-
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    persistedQueries: false,
+    cache: "bounded",
 });
 
 
